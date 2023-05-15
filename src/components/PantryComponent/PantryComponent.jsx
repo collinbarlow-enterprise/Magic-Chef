76ebced97fa3b71f2c12995fc26a0b652c427fe0
CollinBarlow <collinbarlow@gmail.com>
2023-05-15 09:58:37 -0700

    still working on figuring out why there are two api calls

--- src/components/PantryComponent/PantryComponent.jsx
+++ src/components/PantryComponent/PantryComponent.jsx
@@ -15,6 +15,7 @@ export default function PantryComponent({ingredients, id, pantry,  handleEditLis
     getPantries()
 }
   async function handleNavigation() {
+    console.log('how many times does this show up')
     navigate(`/loadingPage/${id}`)
   }
 
